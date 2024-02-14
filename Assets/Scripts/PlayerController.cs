b6e9d789dc5cc7a8eb2114631da75bdde9e8755e
Nicholas Calleja <nick.calleja545@gmail.com>
2024-02-14 15:53:31 -0500

    Working on PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody2D rigbod;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigbod = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
