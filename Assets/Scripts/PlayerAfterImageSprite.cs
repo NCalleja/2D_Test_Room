1e0a3c6751123ff657592597ad7bb7a606ac9964
Nicholas Calleja <nick.calleja545@gmail.com>
2024-05-15 11:38:05 -0400

    Adding Variables to the PlayerAfterImageSprite script

--- Assets/Scripts/PlayerAfterImageSprite.cs
+++ Assets/Scripts/PlayerAfterImageSprite.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class PlayerAfterImageSprite : MonoBehaviour
 {
-    
 
+    private float activeTime = 0.1f;
+    private float timeActivated;
+    private float alpha;
+    private float alphaSet = 0.8f;
+
+    private Transform player;
+
+    private SpriteRenderer SR;
+    private SpriteRenderer playerSR;
+
+    private Color color;
 
 }
