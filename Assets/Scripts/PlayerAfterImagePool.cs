18fda6571fab6bef7ba933723ca54e21d5b4206d
Nicholas Calleja <nick.calleja545@gmail.com>
2024-05-30 18:21:57 -0400

    Reviewing previous changes

--- Assets/Scripts/PlayerAfterImagePool.cs
+++ Assets/Scripts/PlayerAfterImagePool.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerAfterImagePool : MonoBehaviour
 {
 
+    // Serialize Field adds the below field to the Inspector
     [SerializeField]
     private GameObject afterImagePrefab;
 
