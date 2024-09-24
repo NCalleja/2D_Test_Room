02c23f8cd6087e254f658a4b6f0ddcba6d3a7d17
Nicholas Calleja <nick.calleja545@gmail.com>
2024-09-24 11:02:18 -0400

    References for Rigibody2D

--- Assets/Scripts/DeadDummyController.cs
+++ Assets/Scripts/DeadDummyController.cs
@@ -11,7 +11,7 @@ public class DeadDummyController : MonoBehaviour
 
     private PlayerController pc;
     private GameObject aliveDD, brokenHeadDD, brokenTorsoDD, brokenRightArmDD, brokenLeftArmDD, brokenRightLegDD, brokenLeftLegDD;
-    private Rigidbody rbAlive, rbBrokenHead, rbBrokenTorso, rbBrokenRightArm, rbBrokenLeftArm, rbBrokenRightLeg, rbBrokenLeftLeg;
+    private Rigidbody2D rbAlive, rbBrokenHead, rbBrokenTorso, rbBrokenRightArm, rbBrokenLeftArm, rbBrokenRightLeg, rbBrokenLeftLeg;
     private Animator aliveAnim;
 
     private void Start()
@@ -21,7 +21,8 @@ public class DeadDummyController : MonoBehaviour
         // This looks through the heirachy and returns the first game object that it finds that uses "Player"
         pc = GameObject.Find("Test Dummy").GetComponent<PlayerController>();
 
-        // This is looking through the child game objects of the object this script is attached to.
+        // GameObject References
+            // This is looking through the child game objects of the object this script is attached to.
             // We're doing this for ALL of the broken pieces too.
         aliveDD = transform.Find("Alive").gameObject;
         brokenHeadDD = transform.Find("Broken Head").gameObject;
@@ -30,6 +31,20 @@ public class DeadDummyController : MonoBehaviour
         brokenLeftArmDD = transform.Find("Broken Left Arm").gameObject;
         brokenRightLegDD = transform.Find("Broken Right Leg").gameObject;
         brokenLeftLegDD = transform.Find("Broken Left Leg").gameObject;
+
+        // Animator Reference
+        aliveAnim = aliveDD.GetComponent<Animator>();
+
+        // Rigibody References
+        rbAlive = aliveDD.GetComponent<Rigidbody2D>(); 
+        rbBrokenHead = brokenHeadDD.GetComponent<Rigidbody2D>();
+        rbBrokenTorso = brokenTorsoDD.GetComponent<Rigidbody2D>();
+        rbBrokenRightArm = brokenRightArmDD.GetComponent<Rigidbody2D>();
+        rbBrokenLeftArm = brokenLeftArmDD.GetComponent<Rigidbody2D>();
+        rbBrokenRightLeg = brokenRightLegDD.GetComponent<Rigidbody2D>();
+        rbBrokenLeftLeg = brokenLeftLegDD.GetComponent<Rigidbody2D>();
+
+
     }
 
 }
