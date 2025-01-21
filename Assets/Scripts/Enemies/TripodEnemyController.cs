1269bbcd0c7de9f7e4fc07f9eb6d80870cb07d6a
Nicholas Calleja <nick.calleja545@gmail.com>
2025-01-21 17:08:12 -0500

    Created the EnemyTripod Script and a few States for Walking

---
+++ Assets/Scripts/Enemies/TripodEnemyController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TripodEnemyController : MonoBehaviour
+{
+
+    private enum State
+    {
+        Walking,
+        Knockback,
+        Dead
+    }
+
+    private State currentState;
+
+    // -- WALKING STATE --
+
+    private void EnterWalkingState()
+    {
+
+    }
+
+    private void UpdateWalkingState()
+    {
+
+    }
+
+    private void ExitWalkingState()
+    {
+        
+    }
+
+}
