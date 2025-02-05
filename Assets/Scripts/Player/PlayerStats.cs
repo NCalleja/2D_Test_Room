7bc4ffffb08d6af5b35e3e86915c469ca2499948
Nicholas Calleja <nick.calleja545@gmail.com>
2025-02-05 16:14:05 -0500

    Test Dummy now BREAKS!

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -58,7 +58,6 @@ public class PlayerStats : MonoBehaviour
         rbBrokenLeftLeg = broken_LeftLeg.GetComponent<Rigidbody2D>();
         rbBrokenRightLeg = broken_RightLeg.GetComponent<Rigidbody2D>();
 
-
     }
 
     public void DecreaseHealth(float amount)
@@ -90,23 +89,43 @@ public class PlayerStats : MonoBehaviour
     {
         // Must Turn Off Parent
         broken_Head.transform.SetParent(null);
+        broken_Torso.transform.SetParent(null);
+        broken_LeftArm.transform.SetParent(null);
+        broken_RightArm.transform.SetParent(null);
+        broken_LeftLeg.transform.SetParent(null);
+        broken_RightLeg.transform.SetParent(null);
 
         // Setting GameObjects to Active
         broken_Head.SetActive(true);
-        //broken_Torso.SetActive(true);
-        //broken_LeftArm.SetActive(true); 
-        //broken_RightArm.SetActive(true);
-        //broken_LeftLeg.SetActive(true);
-        //broken_RightLeg.SetActive(true);
+        broken_Torso.SetActive(true);
+        broken_LeftArm.SetActive(true); 
+        broken_RightArm.SetActive(true);
+        broken_LeftLeg.SetActive(true);
+        broken_RightLeg.SetActive(true);
 
         // Setting Position to Parent GameObject
         broken_Head.transform.position = transform.position;
+        broken_Torso.transform.position = transform.position;
+        broken_LeftArm.transform.position = transform.position;
+        broken_RightArm.transform.position = transform.position;
+        broken_LeftLeg.transform.position = transform.position;
+        broken_RightLeg.transform.position = transform.position;
 
         // Apply Knockback
         rbBrokenHead.velocity = new Vector2(UnityEngine.Random.Range(-2f, 2f), UnityEngine.Random.Range(2f, 5f));
+        rbBrokenTorso.velocity = new Vector2(UnityEngine.Random.Range(-1.5f, 1.5f), UnityEngine.Random.Range(1f, 3f));
+        rbBrokenLeftArm.velocity = new Vector2(UnityEngine.Random.Range(-3f, -1f), UnityEngine.Random.Range(2f, 4f));
+        rbBrokenRightArm.velocity = new Vector2(UnityEngine.Random.Range(1f, 3f), UnityEngine.Random.Range(2f, 4f));
+        rbBrokenLeftLeg.velocity = new Vector2(UnityEngine.Random.Range(-2f, -0.5f), UnityEngine.Random.Range(1f, 3f));
+        rbBrokenRightLeg.velocity = new Vector2(UnityEngine.Random.Range(0.5f, 2f), UnityEngine.Random.Range(1f, 3f));
 
         // Apply Torque
         rbBrokenHead.AddTorque(UnityEngine.Random.Range(-10, 10f), ForceMode2D.Impulse);
+        rbBrokenTorso.AddTorque(UnityEngine.Random.Range(-5, 5f), ForceMode2D.Impulse);
+        rbBrokenLeftArm.AddTorque(UnityEngine.Random.Range(-7, 7f), ForceMode2D.Impulse);
+        rbBrokenRightArm.AddTorque(UnityEngine.Random.Range(-7, 7f), ForceMode2D.Impulse);
+        rbBrokenLeftLeg.AddTorque(UnityEngine.Random.Range(-5, 5f), ForceMode2D.Impulse);
+        rbBrokenRightLeg.AddTorque(UnityEngine.Random.Range(-5, 5f), ForceMode2D.Impulse);
 
     }
 
